228f565467e56007a26ccdedadcdbdc7f7a39f99
OtakuOracle <nastya.vakhitova700@mail.ru>
2026-03-17 11:19:37 +0300

    init

---
+++ StolyarovaDemo1703/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StolyarovaDemo1703
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
